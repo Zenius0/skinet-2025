d75f7d38c409309bf5e632bfff5e7457b4272a8f
Selim <mustafa2001selim@gmail.com>
2025-08-05 14:45:02 +0300

    API endpoints created

---
+++ skinet/Core/Entities/Product.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Core.Entities;
+
+public class Product : BaseEntity
+{
+    public required String Name { get; set; }
+
+    public required String Description { get; set; }
+
+    public Decimal Price { get; set; }
+
+    public required String PictureUrl { get; set; }
+
+    public required String Type { get; set; }
+
+    public required String Brand { get; set; }
+
+    public int QuantityInStock { get; set; }
+}
+
